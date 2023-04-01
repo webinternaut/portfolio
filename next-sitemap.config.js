1a260dc8a8c43837846e7dbf3da26b87b9cdad04
webinternaut <webinternaut@gmail.com>
2023-03-31 21:52:22 -0400

    Page / Scripts Added: Site-map

---
+++ next-sitemap.config.js
@@ -0,0 +1,1 @@
+/** @type {import('next-sitemap').IConfig} */// Default code you can customize according to your requirements.module.exports = {siteUrl: process.env.SITE_URL || 'https://example.com',generateRobotsTxt: true, // (optional)// REST CODE READ DOCS  ...}
